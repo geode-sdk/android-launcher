a2279f7b2c4232e4c4843a4a0eb47afe9aa64e94
qimiko <25387744+qimiko@users.noreply.github.com>
2022-05-27 22:49:08 -0700

    make cocos2dxhelper static

--- app/src/main/java/org/cocos2dx/lib/Cocos2dxHelper.kt
+++ app/src/main/java/org/cocos2dx/lib/Cocos2dxHelper.kt
@@ -1,44 +1,47 @@
 package org.cocos2dx.lib
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.os.Process
 
 
-@Suppress("unused")
-class Cocos2dxHelper {
-    companion object {
-        //    private val sCocos2dMusic: Cocos2dxMusic? = null
+@Suppress("unused", "KotlinJniMissingFunction")
+object Cocos2dxHelper {
+//    private val sCocos2dMusic: Cocos2dxMusic? = null
 //    private val sCocos2dSound: Cocos2dxSound? = null
-//        private val sAssetManager: AssetManager? = null
-        //    private val sCocos2dxAccelerometer: Cocos2dxAccelerometer? = null
-//        private val sAccelerometerEnabled = false
-        private var sPackageName: String? = null
-        private var sFileDirectory: String? = null
-        @SuppressLint("StaticFieldLeak")
-        private var sContext: Context? = null
-        private var sCocos2dxHelperListener: Cocos2dxHelperListener? = null
+//    private val sAssetManager: AssetManager? = null
+//    private val sCocos2dxAccelerometer: Cocos2dxAccelerometer? = null
+//    private val sAccelerometerEnabled = false
+    private var sPackageName: String? = null
+    private var sFileDirectory: String? = null
 
-        @JvmStatic
-        external fun nativeSetApkPath(pApkPath: String)
+//    private var sContext: Context? = null
+    private var sCocos2dxHelperListener: Cocos2dxHelperListener? = null
 
-        @JvmStatic
-        private external fun nativeSetEditTextDialogResult(pBytes: ByteArray)
+    @JvmStatic
+    external fun nativeSetApkPath(pApkPath: String)
 
-        @JvmStatic
-        fun getCocos2dxWritablePath(): String? {
-            return sFileDirectory
-        }
+    @JvmStatic
+    private external fun nativeSetEditTextDialogResult(pBytes: ByteArray)
 
-        fun init(pContext: Context, pCocos2dxHelperListener: Cocos2dxHelperListener) {
-            val applicationInfo: ApplicationInfo = pContext.applicationInfo
-            sContext = pContext
-            sCocos2dxHelperListener = pCocos2dxHelperListener
-            sPackageName = applicationInfo.packageName
-            sFileDirectory = pContext.filesDir.absolutePath
+    @JvmStatic
+    fun getCocos2dxWritablePath(): String? {
+        return sFileDirectory
+    }
 
-            nativeSetApkPath(applicationInfo.sourceDir)
+    @JvmStatic
+    fun terminateProcess() {
+        Process.killProcess(Process.myPid())
+    }
+
+    fun init(pContext: Context, pCocos2dxHelperListener: Cocos2dxHelperListener) {
+        val applicationInfo: ApplicationInfo = pContext.applicationInfo
+//        sContext = pContext
+        sCocos2dxHelperListener = pCocos2dxHelperListener
+        sPackageName = applicationInfo.packageName
+        sFileDirectory = pContext.filesDir.absolutePath
+
+        nativeSetApkPath(applicationInfo.sourceDir)
 //        Cocos2dxHelper.sCocos2dxAccelerometer = Cocos2dxAccelerometer(pContext)
 //        Cocos2dxHelper.sCocos2dMusic = Cocos2dxMusic(pContext)
 //        var simultaneousStreams: Int = Cocos2dxSound.MAX_SIMULTANEOUS_STREAMS_DEFAULT
@@ -47,14 +50,8 @@ class Cocos2dxHelper {
 //        }
 //        Cocos2dxHelper.sCocos2dSound = Cocos2dxSound(pContext, simultaneousStreams)
 //        Cocos2dxHelper.sAssetManager = pContext.getAssets()
-            Cocos2dxBitmap.setContext(pContext)
+        Cocos2dxBitmap.setContext(pContext)
 //            Cocos2dxETCLoader.setContext(pContext)
-        }
-
-        @JvmStatic
-        fun terminateProcess() {
-            Process.killProcess(Process.myPid())
-        }
     }
 
     interface Cocos2dxHelperListener {
