067c0e87b205f83727338bc54780c7e45ef06e90
qimiko <25387744+qimiko@users.noreply.github.com>
2023-12-30 23:45:08 -0700

    fix settings scroll

--- app/src/main/java/com/geode/launcher/SettingsActivity.kt
+++ app/src/main/java/com/geode/launcher/SettingsActivity.kt
@@ -12,7 +12,9 @@ import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
@@ -112,7 +114,8 @@ fun SettingsScreen(
             Column(
                 Modifier
                     .padding(innerPadding)
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 OptionsGroup(context.getString(R.string.preference_category_testing)) {
