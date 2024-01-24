dcfab5e00119f272139a1464b98f57785cdb8b95
Fleeym <61891787+fleeym@users.noreply.github.com>
2024-01-24 04:40:04 +0200

    fix file utils, move them to a separate class

---
+++ app/src/main/java/com/geode/launcher/activityresult/GeodeSaveFileActivityResult.kt
@@ -0,0 +1,33 @@
+package com.geode.launcher.activityresult
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.DocumentsContract
+import androidx.activity.result.contract.ActivityResultContract
+
+open class GeodeSaveFileActivityResult : ActivityResultContract<GeodeSaveFileActivityResult.SaveFileParams, Uri?>() {
+    class SaveFileParams(val mimeType: String?, val defaultPath: Uri?) {}
+    override fun createIntent(context: Context, input: SaveFileParams): Intent {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+            .setType(input.mimeType ?: "*/*")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (input.defaultPath != null) {
+                intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, input.defaultPath)
+            }
+        }
+        return intent
+    }
+
+    final override fun getSynchronousResult(
+        context: Context,
+        input: SaveFileParams
+    ): SynchronousResult<Uri?>? = null
+
+    final override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+        return intent.takeIf { resultCode == Activity.RESULT_OK }?.data
+    }
+}
+
