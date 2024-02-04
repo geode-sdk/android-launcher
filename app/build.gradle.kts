96354096e2206b2d503589d418679e3ea0d89ead
qimiko <25387744+qimiko@users.noreply.github.com>
2024-02-03 18:08:31 -0700

    minor update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.geode.launcher"
         minSdk = 23
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.2.0"
+        versionCode = 11
+        versionName = "1.2.1"
 
         vectorDrawables {
             useSupportLibrary = true
