299f08191990048aa774c8fe39057892a8f137f1
qimiko <25387744+qimiko@users.noreply.github.com>
2024-02-20 15:08:56 -0700

    increment version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.geode.launcher"
         minSdk = 23
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.3.1"
+        versionCode = 14
+        versionName = "1.3.2"
 
         vectorDrawables {
             useSupportLibrary = true
