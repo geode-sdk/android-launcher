150bac781b6626f8ff5331593233d9351a7885e1
Chloe <25387744+qimiko@users.noreply.github.com>
2025-12-19 02:49:55 -0700

    fix application icon scrolling

--- app/src/main/java/com/geode/launcher/preferences/ApplicationIconActivity.kt
+++ app/src/main/java/com/geode/launcher/preferences/ApplicationIconActivity.kt
@@ -19,8 +19,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -181,7 +183,8 @@ fun ApplicationIconScreen(
         Column(
             Modifier
                 .padding(innerPadding)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.SpaceEvenly,
         ) {
             val selectedIcon by PreferenceUtils.useStringPreference(PreferenceUtils.Key.SELECTED_ICON)
