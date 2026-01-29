36fcbb5c52508066f96c4ff188e4cbaf8854f0c9
Chloe <25387744+qimiko@users.noreply.github.com>
2026-01-29 15:57:24 -0700

    bump launcher version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.geode.launcher"
         minSdk = 23
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.6.1"
+        versionCode = 23
+        versionName = "1.7.0"
 
         vectorDrawables {
             useSupportLibrary = true
