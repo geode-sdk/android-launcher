559cedb0f05bfacdc73fc6e9a4781ab8f8ec0a26
Chloe <git@celosia.zip>
2026-03-12 17:35:18 -0700

    add symbols to blocklist

--- app/src/main/cpp/base.h
+++ app/src/main/cpp/base.h
@@ -15,7 +15,7 @@ public:
 };
 
 // this is every function that i thought would be relevant
-constexpr std::array<const std::string_view, 10> remove_symbols{
+constexpr std::array<const std::string_view, 12> remove_symbols{
         "__gxx_personality_v0",
         "__cxa_throw",
         "__cxa_rethrow",
@@ -25,7 +25,9 @@ constexpr std::array<const std::string_view, 10> remove_symbols{
         "__cxa_guard_abort",
         "__cxa_guard_acquire",
         "__cxa_guard_release",
-        "__cxa_free_exception"
+        "__cxa_free_exception",
+        "__emutls_get_address", // added in 2.208
+        "__cxa_thread_atexit"
 
         // android 10's libc doesn't have these symbols
         // and the ndk is forbidden from exporting them!! so keep it for old mods...
