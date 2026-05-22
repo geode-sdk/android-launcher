931a3b7ff308027d39fd6560558dcd97d99c5993
VicDash78 <vicdash.78@gmail.com>
2026-05-22 19:51:22 -0400

    Update PACKAGE_NAME constant for application

--- app/src/main/java/com/geode/launcher/utils/Constants.kt
+++ app/src/main/java/com/geode/launcher/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.geode.launcher.utils
 import android.annotation.SuppressLint
 
 object Constants {
-    const val PACKAGE_NAME = "com.robtopx.geometryjump"
+    const val PACKAGE_NAME = "com.vicdash78.wgdpsv2208"
 
     const val COCOS_LIB_NAME = "cocos2dcpp"
     const val FMOD_LIB_NAME = "fmod"
